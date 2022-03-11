6837811adca312a64dc9cec31100b18fc732d87a
Emanuel Evans <evanseman@ecs.vuw.ac.nz>
2022-03-11 21:08:09 +1300

    Add known vulnerability
    This is to test that dependabot will pick it up

--- src/main/java/JsonExample.java
+++ src/main/java/JsonExample.java
@@ -1,17 +1,18 @@
 import org.json.JSONObject;
 
 public class JsonExample {
-    private static void createJSON(boolean prettyPrint) {
-        JSONObject tomJsonObj = new JSONObject();
-        tomJsonObj.put("name", "Mick");
-        tomJsonObj.put("age", 25);
+    public static JSONObject createJSON() {
+        JSONObject person = new JSONObject();
+        person.put("name", "Mick");
+        person.put("age", 25);
 
-        tomJsonObj.put("favorite_foods", new String[]{"Prosciutto", "Papardelle", "Brezel"});
+        person.put("favorite_foods", new String[]{"Prosciutto", "Papardelle", "Brezel"});
 
-        System.out.println(tomJsonObj.toString(4));
+        return person;
     }
 
     public static void main(String[] args) {
-        createJSON(true);
+
+        System.out.println(createJSON().toString(4));
     }
 }
