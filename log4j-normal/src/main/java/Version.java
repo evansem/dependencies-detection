2e8db39c2dacd362ca280dc25e5dfde53b401023
Emanuel Evans <evanseman@ecs.vuw.ac.nz>
2022-09-25 09:48:07 +1200

    Add custom clone-like classes for the other samples

---
+++ log4j-normal/src/main/java/Version.java
@@ -0,0 +1,11 @@
+//public class Version {
+//
+//    public static void main(final String[] args) {
+//        System.out.println(getProductString());
+//    }
+//
+//    public static String getProductString() {
+//        return String.format("This method is different but still uses .format");
+//    }
+//
+//}
---
+++ log4j-normal/src/main/java/empty/Version.java
@@ -0,0 +1,13 @@
+package empty;
+
+public class Version {
+
+    public static void main(final String[] args) {
+        return;
+    }
+
+    public static String getProductString() {
+        return null;
+    }
+
+}
